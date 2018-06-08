07065349acf7bfcd12b55e64daff7e20e7763cc6
Gal Topper <galt@iguaz.io>
2018-06-08 19:46:34 +0800

    Support reducing partition depth by using prefixing instead.

--- README.md
+++ README.md
@@ -16,6 +16,9 @@ Put to KV
 * Run `echo '{"x":"abc", "date":"2018-06-01 10:01:55"}' | java -cp igz/bigdata/libs/scala-library-2.11.12.jar:igz/bigdata/libs/v3io-spark2-object-dataframe_2.11.jar:kv_cli-assembly-0.1.jar io.iguaz.cli.kv.PutJson /test x date '(\d\d\d\d)-(\d\d)-(\d\d) (\d\d):(\d\d):(\d\d)' year/month/day/hour/minute/second`.
   * This will write an entry to `/test/year=2018/month=06/day=01/hour=10/minute=01/second=55/abc`.
   * The date is partitioned by the parsed values of the date field, prefixed by the respective partition names.
+* Run `echo '{"x":"abc", "date":"2018-06-01 10:01:55"}' | java -cp igz/bigdata/libs/scala-library-2.11.12.jar:igz/bigdata/libs/v3io-spark2-object-dataframe_2.11.jar:kv_cli-assembly-0.1.jar io.iguaz.cli.kv.PutJson /test x date '(\d\d\d\d)-(\d\d)-(\d\d) (\d\d):(\d\d):(\d\d)' year/month/day/hour minute,second`.
+  * This will write an entry to `/test/year=2018/month=06/day=01/hour=10/minute=01_second=55_abc`.
+  * The date is partitioned by the parsed values of the date field, excluding minute and hour, prefixed by the respective partition names. The key is prefixed by the minute and second.
 * Run `echo '{"x":"abc", "date":"2018-06-01 10:01:55"}' | java -cp igz/bigdata/libs/scala-library-2.11.12.jar:igz/bigdata/libs/v3io-spark2-object-dataframe_2.11.jar:kv_cli-assembly-0.1.jar io.iguaz.cli.kv.PutJson /test x date '(\d\d\d\d)-(\d\d)-(\d\d) (\d\d):(\d\d):(\d\d)'`.
   * This will write an entry to `/test/test/2018/06/01/10/01/55/abc`.
   * The date is partitioned by the parsed values of the date field.
@@ -28,5 +31,6 @@ Put to KV
   
 Configure
 ----------
-* Use `-Dmode` to set the overwrite mode (e.g. `-Dmode=replace`).
-* Use `-Dcontainer` to choose a container (e.g. `-Dcontainer=bigdata`).
+* Use `-Dmode` to set the overwrite mode (e.g. `-Dmode=replace`). Default is `overwrite`.
+* Use `-Dcontainer` to choose a container (e.g. `-Dcontainer=bigdata`). Default is system default.
+* Use `-Dkey-prefix-separator` to choose a container (e.g. `-Dkey-prefix-separator=.`). Default is `_`.
--- src/main/scala/io/iguaz/cli/kv/PutJson.scala
+++ src/main/scala/io/iguaz/cli/kv/PutJson.scala
@@ -16,32 +16,35 @@ object PutJson {
   def main(args: Array[String]): Unit = {
     TerminateOnSigPipe.registerHandler()
     require(
-      args.length >= 2 && args.length <= 5,
-      s"Usage: java ${getClass.getName.dropRight(1)} <target> <key field> " +
-        s"[<partition field> [<partition regex> [<partition names>]]]"
+      args.length >= 2 && args.length <= 6,
+      s"Usage: java ${getClass.getName.dropRight(1)} <target> <key field> [<partition field> [<partition regex> " +
+        s"[<partition name>/<partition name>/...] [<partition prefix>,<partition prefix>,...]]]"
     )
     val targetBase = if (args(0).endsWith("/")) args(0) else s"${args(0)}/"
     val keyField = args(1)
     val partitionFieldOption = args.lift.apply(2)
     val partitionRegexOption = partitionFieldOption.flatMap(_ => args.lift.apply(3).map(_.r))
     val partitionNamesOption = partitionFieldOption.flatMap(_ => args.lift.apply(4).map(_.split('/').toVector))
+    val keyPrefixes = partitionNamesOption.flatMap(_ => args.lift.apply(5).map(_.split(',').toVector))
+      .getOrElse(Vector.empty)
 
     val container = sys.props.getOrElse("container", "")
     val overwriteMode = sys.props.get("mode")
       .map(str => OverwriteMode.valueOf(str.toUpperCase))
       .getOrElse(OverwriteMode.OVERWRITE)
+    val keyPrefixSeparator = sys.props.getOrElse("key-prefix-separator", "_")
 
     val targetBaseUri = new URI("v3io", container, targetBase, null, null)
     val inputLineIterator = Source.fromInputStream(System.in).getLines()
     val updateEntryIterator = inputLineIterator.map { line =>
       val json = JsonMethods.parse(line)
       val key = (json \ keyField).extract[Any].toString
-      val targetUri = partitionFieldOption match {
-        case None => targetBaseUri
+      val (targetUri, keyPrefix) = partitionFieldOption match {
+        case None => targetBaseUri -> ""
         case Some(partitionField) =>
           val partitionFieldValueStr = (json \ partitionField).extract[Any].toString
           partitionRegexOption match {
-            case None => targetBaseUri.resolve(partitionFieldValueStr)
+            case None => targetBaseUri.resolve(partitionFieldValueStr) -> ""
             case Some(partitionRegex) =>
               val partitionValues = partitionFieldValueStr match {
                 case partitionRegex(subgroups@_*) => subgroups
@@ -49,26 +52,28 @@ object PutJson {
                   s"Partition regex '$partitionRegex' did not match '$partitionFieldValueStr'."
                 )
               }
-              val partitions = partitionNamesOption match {
-                case None => partitionValues
+              val (partitions, prefix) = partitionNamesOption match {
+                case None => partitionValues -> ""
                 case Some(partitionNames) =>
                   require(
-                    partitionNames.size == partitionValues.size,
+                    partitionNames.size + keyPrefixes.size == partitionValues.size,
                     s"Captured ${partitionValues.size} parititons in '$partitionFieldValueStr', " +
                       s"but ${partitionNames.size} were named."
                   )
-                  partitionNames.zip(partitionValues).map { case (k, v) => s"$k=$v" }
+                  partitionNames.zip(partitionValues).map { case (k, v) => s"$k=$v" } ->
+                    keyPrefixes.zip(partitionValues.drop(partitionNames.size))
+                      .map { case (k, v) => s"$k=$v" }.mkString(keyPrefixSeparator)
               }
               partitions.foldLeft(targetBaseUri) {
                 case (acc, nextPartition) => acc.resolve(s"$nextPartition/")
-              }
+              } -> prefix
           }
       }
       val data = json.extract[Map[String, Any]].mapValues {
         case bigInt: BigInt => bigInt.toLong
         case other => other
       }
-      val row = Row(key, data)
+      val row = Row(if (keyPrefix.isEmpty) key else s"${keyPrefix}_$key", data)
       UpdateEntry(targetUri, row, overwriteMode)
     }
     KeyValueOperations().updateItems(updateEntryIterator)
